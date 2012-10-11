65348d544bb4ced9c326247c3518c8be2654cdb1
Alessandro Glielmi <arkantiko@gmail.com>
2012-10-11 13:02:19 +0200

    Added some choice

--- scan.sh
+++ scan.sh
@@ -1,17 +1,38 @@
 #!/bin/sh
 
-if [ $1 == c ]
+local if_name=$(cat /proc/net/dev | sed -n -e 's/:.*//' -e 's/[ /t]*//' -e '/^wlan[0-9]$/p')
+
+if [ $1 == ${if_name} ]
 	then
-		read $2
-		iwinfo wlan0 s|grep -A3 -B3 "Channel: ${2}"
-elif [ $1 != c ]
-	then
-		iwinfo wlan0 s|grep -A4 -B2 "$1"
-elif [ -z $1 ]
-	then
-		echo "Insert SSID [ENTER]"
-		read ssid
-		iwinfo wlan0 s|grep -A4 -B2 $ssid
+		if [ $2 == c ]                                                 
+			then                                                   
+				iwinfo ${if_name} s|grep -A3 -B3 "Channel: ${3}"
+		elif [ $2 != c ]                                    
+			then                                        
+				iwinfo ${if_name} s|grep -A4 -B2 "$2"
+		elif [ -z $2 ]                                       
+			then                                         
+				echo "Insert SSID [ENTER]"           
+				read ssid                            
+				iwinfo ${if_name} s|grep -A4 -B2 $ssid
+		fi
+	else
+		echo "Your wifi interface: '${if_name}'"
+		echo "Insert interface name [ENTER]"
+		read if_sel
+
+		if [ $1 == c ]
+			then
+				iwinfo ${if_sel} s|grep -A3 -B3 "Channel: ${2}"
+		elif [ $1 != c ]
+			then
+				iwinfo ${if_sel} s|grep -A4 -B2 "$1"
+		elif [ -z $1 ]
+			then
+				echo "Insert SSID [ENTER]"
+				read ssid
+				iwinfo ${if_sel} s|grep -A4 -B2 $ssid
+		fi
 fi
 
 exit 0
